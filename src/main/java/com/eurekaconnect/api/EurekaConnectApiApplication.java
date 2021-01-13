e20b9ed4f32c0978633a26102d8fae030a5b57ab
Ryder Kishan <kishan020696@gmail.com>
2021-01-13 11:08:53 +0530

    Spring Initializer

---
+++ src/main/java/com/eurekaconnect/api/EurekaConnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.eurekaconnect.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConnectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurekaconnect/api/EurekaConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaconnect.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

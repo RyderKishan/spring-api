5f7bc7c9b47e0c1b52e5b213b7b90264b7ac2c35
Ryder Kishan <kishan020696@gmail.com>
2021-02-24 10:46:46 +0530

    (update): 2.4.3

--- src/main/java/com/eurekaconnect/api/EurekaConnectApi.java
+++ src/main/java/com/eurekaconnect/api/EurekaConnectApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaConnectApiApplication {
+public class EurekaConnectApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaConnectApiApplication.class, args);
+		SpringApplication.run(EurekaConnectApi.class, args);
 	}
 
 }
--- src/test/java/com/eurekaconnect/api/EurekaConnectApiTests.java
+++ src/test/java/com/eurekaconnect/api/EurekaConnectApiTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaConnectApiApplicationTests {
+class EurekaConnectApiTests {
 
 	@Test
 	void contextLoads() {
